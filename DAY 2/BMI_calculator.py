8711b2d3b65efe45f261a03a4026ebfc5d396b40
Divyanshu <dvskmt92@gmail.com>
2025-09-01 22:13:58 +0530

    little modificaitons

--- DAY 2/BMI_calculator.py
+++ DAY 2/BMI_calculator.py
@@ -12,3 +12,10 @@ print("Your BMI is: " + str(round(BMI)))
 print("Your BMI is: " + str(round(BMI, 2)))
 
 print(f"Your BMI is: {BMI:.2f}")
+
+if BMI < 18.5:
+    print(f"{BMI:.2f} means you are malnourished.\nPlease take care if yourself.")
+elif BMI < 24.9:
+    print(f"{BMI:.2f} means you are healthy.\nKeep up the good work")
+else:
+    print(f"{BMI:.2f} means you are overweight.\nKindly change your diet.")
